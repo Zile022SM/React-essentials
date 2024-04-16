bfe411be4c4c3f7a3d47d857cb648237f9688822
Zile022SM <zile.kuzminac85@gmail.com>
2024-04-16 19:27:27 +0200

    Add dynamic content usig state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header/Header";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
+import { useState } from "react";
 
 
 
 function App() {
+
+  const [tabContent, setTabContent] = useState('components');
+
+  function handleSelect(selectedButton){
+       setTabContent(selectedButton);
+  }
 
   return (
     <div>
@@ -27,6 +36,27 @@ function App() {
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
 
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
+          </menu>
+          
+          <div id="tab-content">
+            <h3>{EXAMPLES[tabContent].title}</h3>
+            <p>{EXAMPLES[tabContent].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[tabContent].code}
+              </code>
+            </pre>
+          </div>
+          {tabContent}
         </section>
       </main>
     </div>
